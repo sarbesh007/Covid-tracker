e726e7cd16d87af8c20cad5009616be2214ef9fa
Sarbesh <sarbesh007@gmail.com>
2020-05-03 11:56:52 +0530

    Updates

---
+++ precache-manifest.b02f1b434fb90e100e00cf71e612a8b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd4f00468b8f6c700a50eb5f61d2984",
+    "url": "/Covid-tracker/index.html"
+  },
+  {
+    "revision": "480e3f8d61276101b9aa",
+    "url": "/Covid-tracker/static/css/main.799f7649.chunk.css"
+  },
+  {
+    "revision": "f19a841d5915fad79c6c",
+    "url": "/Covid-tracker/static/js/2.be1860db.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-tracker/static/js/2.be1860db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480e3f8d61276101b9aa",
+    "url": "/Covid-tracker/static/js/main.d7c7d63f.chunk.js"
+  },
+  {
+    "revision": "79fd60d326e38f4b7ce4",
+    "url": "/Covid-tracker/static/js/runtime-main.68414a74.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-tracker/static/media/image.d7265326.png"
+  }
+]);
